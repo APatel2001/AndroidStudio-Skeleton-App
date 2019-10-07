3d725e1933c901a3cae6ffbb708339e44650b491
Avi Patel <526473+gb96@users.noreply.github.com>
2019-10-06 19:28:23 -0700

    login and template

---
+++ Templates4Success_Master/app/src/main/java/com/example/templates4success/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.templates4success;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+    public void onClick(View view) {
+        Intent i = new Intent(this, EssayType.class);
+        final EditText enterEmail = (EditText) findViewById(R.id.enterEmail);
+        final EditText enterPassword = (EditText) findViewById(R.id.enterPassword);
+        String emailAddress = enterEmail.getText().toString();
+        String password = enterPassword.getText().toString();
+        //String correctEmail = "avidpatel1@gmail.com";
+        //String correctPassword = "college";
+
+        startActivity(i);
+
+
+
+    }
+
+
+
+
+}
