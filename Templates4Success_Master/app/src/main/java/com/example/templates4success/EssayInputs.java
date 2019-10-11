2c8825259a12f4f68eed139dd90fc4d0822a0c5c
Sujal Nahata <sujalnahata@gmail.com>
2019-10-11 06:34:28 -0700

    intents

--- Templates4Success_Master/app/src/main/java/com/example/templates4success/EssayInputs.java
+++ Templates4Success_Master/app/src/main/java/com/example/templates4success/EssayInputs.java
@@ -1,8 +1,11 @@
 package com.example.templates4success;
 
+import android.content.Intent;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EssayInputs extends AppCompatActivity {
 
@@ -10,6 +13,11 @@ public class EssayInputs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_essay_inputs);
+    }
+
+    public void enterButtonClick(View view) {
+        Intent i = new Intent(this, EssayOutputs.class);
+        startActivity(i);
     }
 
 }
