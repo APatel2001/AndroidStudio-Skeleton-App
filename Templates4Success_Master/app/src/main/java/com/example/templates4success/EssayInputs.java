583ee6cb1df92f873556960417c481bd76d1650b
Avi Patel <526473+gb96@users.noreply.github.com>
2019-10-11 20:38:41 -0700

    output works

--- Templates4Success_Master/app/src/main/java/com/example/templates4success/EssayInputs.java
+++ Templates4Success_Master/app/src/main/java/com/example/templates4success/EssayInputs.java
@@ -21,6 +21,54 @@ public class EssayInputs extends AppCompatActivity {
 
     public void enterButtonClick(View view) {
         Intent i = new Intent(this, EssayOutputs.class);
+        Bundle extras = new Bundle();
+
+        final EditText yourname = (EditText) findViewById(R.id.yourname);
+        String name = yourname.getText().toString();
+        extras.putString("Name", name);
+
+
+        final EditText adjective1 = (EditText) findViewById(R.id.adjective1);
+        String Adjective1 = adjective1.getText().toString();
+        extras.putString("Adjective1", Adjective1);
+
+        final EditText adjective2 = (EditText) findViewById(R.id.adjective2);
+        String Adjective2 = adjective2.getText().toString();
+        extras.putString("Adjective2", Adjective2);
+
+        final EditText school = (EditText) findViewById(R.id.school);
+        String School = school.getText().toString();
+        extras.putString("School", School);
+
+        final EditText club = (EditText) findViewById(R.id.club);
+        String Club = club.getText().toString();
+        extras.putString("Club", Club);
+
+        final EditText responsibilities = (EditText) findViewById(R.id.responsibilities);
+        String Responsibilities = responsibilities.getText().toString();
+        extras.putString("Responsibilities", Responsibilities);
+
+        final EditText achievements = (EditText) findViewById(R.id.achievements);
+        String Achievements = achievements.getText().toString();
+        extras.putString("Achievements", Achievements);
+
+        final EditText passion = (EditText) findViewById(R.id.passion);
+        String Passion = passion.getText().toString();
+        extras.putString("Passion", Passion);
+
+        final EditText story = (EditText) findViewById(R.id.story);
+        String Story = story.getText().toString();
+        extras.putString("Story", Story);
+
+        final EditText revelation = (EditText) findViewById(R.id.revelation);
+        String Revelation = revelation.getText().toString();
+        extras.putString("Revelation", Revelation);
+
+        final EditText skills = (EditText) findViewById(R.id.skills);
+        String Skills = skills.getText().toString();
+        extras.putString("Skills", Skills);
+
+        i.putExtras(extras);
         startActivity(i);
     }
     public void settings(View view) {
--- Templates4Success_Master/app/src/main/java/com/example/templates4success/EssayOutputs.java
+++ Templates4Success_Master/app/src/main/java/com/example/templates4success/EssayOutputs.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 public class EssayOutputs extends AppCompatActivity {
@@ -12,6 +13,27 @@ public class EssayOutputs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_essay_outputs2);
+        Bundle extras = getIntent().getExtras();
+        if (extras == null) {
+            return;
+        }
+        String name = extras.getString("Name");
+        String Adjective1 = extras.getString("Adjective1");
+        String Adjective2 = extras.getString("Adjective2");
+        String School = extras.getString("School");
+        String Club = extras.getString("Club");
+        String Responsibilities = extras.getString("Responsibilities");
+        String Achievements = extras.getString("Achievements");
+        String Passion = extras.getString("Passion");
+        String Story = extras.getString("Story");
+        String Revelation = extras.getString("Revelation");
+        String Skills = extras.getString("Skills");
+        String Essay = "Hello, I’m " + name + ". I am a " + Adjective1 + " and " + Adjective2  + " student at " + School + ". I am an avid member of " + Club + ". My responsibilities include " + Responsibilities + ". Because of my hard work, I have been " + Achievements + ". I am also very passionate in " + Passion + ". Once, I " + Story + ". I learned that I am very " + Revelation + " and that I can " + Skills + ".";
+
+        final TextView essay = (TextView) findViewById(R.id.essay);
+        essay.setText(Essay);
+
+
     }
     public void settings(View view) {
         Intent i = new Intent(this, Settings.class);
